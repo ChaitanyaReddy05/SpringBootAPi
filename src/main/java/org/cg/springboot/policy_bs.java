b9b455e4c81f57d39963a6a08f9e198d2da21ff3
ChaitanyaReddy05 <49510138+ChaitanyaReddy05@users.noreply.github.com>
2021-08-23 12:32:18 +0530

    Update policy_bs.java

--- src/main/java/org/cg/springboot/policy_bs.java
+++ src/main/java/org/cg/springboot/policy_bs.java
@@ -12,8 +12,8 @@ public class policy_bs {
     private List<policy_details> Policy_details = new ArrayList<>(Arrays.asList(
             new policy_details("PolicyA","Insurance for everyone","10000","10 years"),
                 new policy_details("PolicyB","Insurance for older","10000","15 years"),
+                new policy_details("PolicyC","Insurance for younger","10000","20 years"),
                 new policy_details("PolicyC","Insurance for younger","10000","20 years")
-        new policy_details("PolicyC","Insurance for younger","10000","20 years")
                 ));
     public List<policy_details> allpolicies(){
         return Policy_details;
