2e4492a501c85f4d4f2238081a71d7bb87da5c08
ChaitanyaReddy05 <49510138+ChaitanyaReddy05@users.noreply.github.com>
2021-08-24 15:04:35 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,17 @@
+import json
+
+f = open('data.json')
+
+data = json.load(f)
+
+def check_for_vulnerabilities():
+    for x in data['imageScanFindings']['findings']:
+        if x['severity'] == 'CRITICAL' or x['severity'] == 'HIGH' or x['severity'] == 'MEDIUM':            
+            f.close()
+            return True
+    f.close()
+    return False
+
+if __name__== '__main__':
+    check_for_vulnerabilities()
 
