d071453e0c1dedb9af0ca34ab316a888cf392d1f
ChaitanyaReddy05 <49510138+ChaitanyaReddy05@users.noreply.github.com>
2021-08-28 14:30:16 +0530

    Update start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd SpringBootAPIProject/release/
+cd release/
 cd $(ls -d */|head -n 1)
-nohup java -jar release.jar & echo $! > ./pid.file &
+nohup java -jar release.jar --server.port=8083 & echo $! > ./pid.file &
