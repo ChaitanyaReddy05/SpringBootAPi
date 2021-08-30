ffbee4deb9cde7bdc6e55a08149318e3fa19d5a8
Kushal-Karmakar <63475722+Kushal-Karmakar@users.noreply.github.com>
2021-08-30 16:26:40 +0530

    Added API Validation Script in Scripts/tests.sh

--- Scripts/tests.sh
+++ Scripts/tests.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-sleep 30
-curl -f -s -I "http://localhost:8083/hello" &>/dev/null && echo OK || echo FAIL
+echo "============================================================================================"
+echo "|                                  API Validation is being started                         |"
+echo "============================================================================================"
+URL1_STATUS_CODE=$(curl -LI http://localhost:8083/hello -o /dev/null -w '%{http_code}\n' -s)
+if [ $URL1_STATUS_CODE -ne 200 ];then
+	echo "|                                  API Validation : Failed                                 |"
+	echo "============================================================================================"
+    exit 1
+fi
+URL2_STATUS_CODE=$(curl -LI http://localhost:8083/allpolicies -o /dev/null -w '%{http_code}\n' -s)
+if [ $URL2_STATUS_CODE -ne 200 ];then
+	echo "|                                  API Validation : Failed                                 |"
+	echo "============================================================================================"
+    exit 1
+fi
+URL3_STATUS_CODE=$(curl -LI http://localhost:8083/policy/PolicyA -o /dev/null -w '%{http_code}\n' -s)
+if [ $URL3_STATUS_CODE -ne 200 ];then
+	echo "|                                  API Validation : Failed                                 |"
+	echo "============================================================================================"
+    exit 1
+fi
+echo "|                                  API Validation : Passed                                 |"
+echo "============================================================================================"
