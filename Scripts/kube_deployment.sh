e2bb6e941b645dfefb09acf183d5375540eae009
Kushal-Karmakar <63475722+Kushal-Karmakar@users.noreply.github.com>
2021-09-02 13:50:03 +0530

    Update kube_deployment.sh

--- Scripts/kube_deployment.sh
+++ Scripts/kube_deployment.sh
@@ -1,2 +1,5 @@
+export ENVIRONMENT=$1
+export NAMESPACE=$2
+export DOCKER_TAG=$3
 envsubst < ./kube-manifests/service.yaml | kubectl apply -f -
 envsubst < ./kube-manifests/deployment.yaml | kubectl apply -f -
